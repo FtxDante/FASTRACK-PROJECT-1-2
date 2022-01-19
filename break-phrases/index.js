9a2aa09b6b6570443809645cb872cbbb2a5e6d19
FtxDante <blog.anutrof@gmail.com>
2022-01-19 17:41:51 -0300

    ref break-sentences now have less code

--- break-phrases/index.js
+++ break-phrases/index.js
@@ -1,7 +1,6 @@
 const breakSentences = (phrase) => {
   const splited = phrase.split(/(\.*\d*x.*.\d+\,\d*.*)/); 
-  const result = splited.filter((item) => item.trim())
-  return result;
+  return splited.filter((item) => item.trim());
 };
 
 module.exports = breakSentences;
